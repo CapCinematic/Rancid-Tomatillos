a99ba2081525d21c684a7ee2347ef57fad8bf126
Isaiah Richardson <capcinematic@gmail.com>
2023-05-21 12:42:34 -0600

    add/stub-data-cypress

--- rancid_tomatillos/cypress/e2e/spec.cy.js
+++ rancid_tomatillos/cypress/e2e/spec.cy.js
@@ -14,30 +14,18 @@ describe("Moldy Mangos", () => {
     cy.contains("Wakanda Forever").should("exist");
   });
 
-  // it('clicks the link "Movie"', () => {
-
-  //   cy.contains('Movie')
-  // })
-
-  // it('clicking "Movie" navigates to "SingleMovie" URL ', () => {
-
-  //   // cy.contains('Movie').click()
-  //   cy.url().should('include', "3000")
-  // })
-
+  
   it("should have additional details about that movie when clicked ", () => {
     cy.get(".cards").first().click();
     cy.contains("Nearly 5,000 years after");
   });
-
+  
   it("user clicks on a movie and the details page is rendered, the URL updates to reflect that movie’s unique ID as well", () => {
     cy.get(".cards").first().click();
     cy.url().should("match", /436270/);
   });
+  
+  it("should be able to test stub data if API is down", () => {
+    cy.intercept("GET", "**/movies/**", {fixture: "movie.json"})
+  });
 });
-
-// As a user, when I load the application, I can see the title of the application CHECKED
-// As a user, when I load the application, I can see a collection of movies. CHECKED
-// As we hover over in cypress it shows the movies we have checked
-// As a user, when I click on a movie, I’m shown additional details about that movie
-// When a user clicks on a movie and the details page is rendered, the URL updates to reflect that movie’s unique ID as well
